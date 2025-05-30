4ed5a617521d5f511b89deafaeb89adaf83d34bb
redkor <redkor910.97@gmail.com>
2025-05-30 13:14:33 +0200

    feat: created label endpoint

---
+++ backend/src/routes/label/index.ts
@@ -0,0 +1,61 @@
+import { Router } from 'express';
+import { PrismaClient } from '../../../generated/prisma';
+import { Request, Response, NextFunction } from 'express';
+
+const router = Router();
+
+const prisma = new PrismaClient();
+
+router.get(
+  '/:itemId',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await prisma.label.findMany({
+        where: {
+          Item: {
+            some: {
+              id: req.params.itemId,
+            },
+          },
+        },
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { bgColor, textColor, name } = req.body;
+    await prisma.label.create({
+      data: {
+        bgColor,
+        textColor,
+        name,
+      },
+    });
+    res.status(200).json();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete(
+  '/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await prisma.label.delete({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default router;
