9f8e4d9c3d90fb3b81c2cd12bc3d69e15dabd447
redkor <redkor910.97@gmail.com>
2025-07-02 13:16:38 +0200

    feat: created list endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import typeRouter from './type/index.js';
 import labelRouter from './label/index.js';
 import itemRouter from './item/index.js';
+import listRouter from './list/index.js';
 
 const router = Router();
 
 router.use('/types', typeRouter);
 router.use('/labels', labelRouter);
 router.use('/items', itemRouter);
+router.use('/lists', listRouter);
 
 export default router;
